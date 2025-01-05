79e9d07c403304d00f86810ae5aa52a52a30bd09
Lucas Pichette <lucascpichette@gmail.com>
2025-01-05 15:25:11 -0800

    enables experimental custom plugin and expands support for banana plugin development

--- init.lua
+++ init.lua
@@ -2,9 +2,13 @@ require("config.lazy")
 require("config.highlights")
 require("config.mappings")
 require("config.ui")
-local bookmarks = require("custom_bookmark.bookmarks")
-bookmarks.setup()
+require("filetypes")
+require("custom_plugins.silver_search.lua.init").setup()
+
+-- local bookmarks = require("custom_bookmark.bookmarks")
+-- bookmarks.setup()
 -- setup calls key_bind and autocmd
 
+vim.opt.signcolumn = "yes:1"
 vim.g.editorconfig = false
 vim.opt.clipboard = "unnamedplus"
