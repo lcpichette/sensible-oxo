e34481b892852aaaaf68ba01e825831837c7e131
Lucas Pichette <lucascpichette@gmail.com>
2025-01-05 16:07:56 -0800

    support `/` working in non-standard buffers

--- lua/custom_search.lua
+++ lua/custom_search.lua
@@ -5,21 +5,37 @@ local utils = fzf.utils
 local actions = fzf.actions
 
 function M.searchFile()
-  fzf.lgrep_curbuf({
-    actions = {
-      ["enter"] = function(sel, opts)
-        actions.file_edit(sel, opts)
-        local last_query = opts.last_query
+  local current_file = vim.api.nvim_buf_get_name(0)
+  if current_file == "" or vim.fn.filereadable(current_file) == 0 then
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    fzf.fzf_exec(lines, {
+      prompt = "Search buffer > ",
+      actions = {
+        ["enter"] = function(selected)
+          local line_num = vim.fn.search(vim.fn.escape(selected[1], "\\/.*$^~[]"), "nw")
+          if line_num > 0 then
+            vim.api.nvim_win_set_cursor(0, { line_num, 0 })
+          end
+        end,
+      },
+    })
+  else
+    fzf.lgrep_curbuf({
+      actions = {
+        ["enter"] = function(sel, opts)
+          actions.file_edit(sel, opts)
+          local last_query = opts.last_query
 
-        if not last_query or #last_query == 0 then
-          return
-        end
+          if not last_query or #last_query == 0 then
+            return
+          end
 
-        local regex = utils.regex_to_magic(opts.last_query)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("/" .. regex .. "<CR>N", true, false, true), "n", true)
-      end,
-    },
-  })
+          local regex = utils.regex_to_magic(opts.last_query)
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("/" .. regex .. "<CR>N", true, false, true), "n", true)
+        end,
+      },
+    })
+  end
 end
 
 function M.liveRipGrep(opts)
