d1b21b67af29190b34f092d11bac84c153a8d9dc
Lucas Pichette <lucascpichette@gmail.com>
2025-01-08 00:32:51 -0800

    Adds markview and neogit

---
+++ lua/plugins/utility.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    config = function()
+      require("markview")
+
+      vim.api.nvim_set_hl(0, "MarkviewHeading", { fg = "#3ddbd9", bold = true })
+      vim.api.nvim_set_hl(0, "MarkviewCodeBlock", { fg = "#78a9ff", bg = "#262626" })
+      vim.api.nvim_set_hl(0, "MarkviewLink", { fg = "#ee5396", underline = true })
+      vim.api.nvim_set_hl(0, "MarkviewBold", { fg = "#f2f4f8", bold = true })
+      vim.api.nvim_set_hl(0, "MarkviewItalic", { fg = "#f2f4f8", italic = true })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+          vim.cmd("MarkviewToggle") -- Automatically toggle markview in markdown
+        end,
+      })
+    end,
+    cmd = { "MarkviewOpen", "MarkviewToggle" },
+  },
+}
